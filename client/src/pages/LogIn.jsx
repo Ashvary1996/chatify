d83dd8443c42648913dc207c9517b61975965ddf
Ashvary Gidian <ashvarygidian1996@gmail.com>
2025-02-26 01:01:32 +0530

    login and sigup layout ready

---
+++ client/src/pages/LogIn.jsx
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+function LogIn() {
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState({
+    email: "",
+    phoneNumber: "",
+    password: "",
+  });
+
+  const [loginWith, setLoginWith] = useState("email");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submit Clicked", userData);
+    toast.success("Log-In Successfully");
+    // redirect to home when user verify
+    navigate("/home");
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-blue-200">
+      <div className="bg-white shadow-lg rounded-xl p-8 w-96">
+        <h2 className="text-2xl font-semibold text-gray-700 text-center mb-6">
+          ✍️chatify
+        </h2>
+
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          {loginWith === "email" ? (
+            <input
+              type="email"
+              placeholder="Email"
+              className="px-4 py-2 border rounded-lg focus:ring-1 focus:ring-blue-800 outline-none"
+              required
+              name="email"
+              value={userData.email}
+              onChange={handleChange}
+            />
+          ) : (
+            <input
+              type="text"
+              placeholder="Phone Number"
+              className="px-4 py-2 border rounded-lg focus:ring-1 focus:ring-blue-800 outline-none"
+              required
+              name="phoneNumber"
+              value={userData.phoneNumber}
+              onChange={handleChange}
+              onInput={(e) => {
+                e.target.value = e.target.value.replace(/\D/g, "").slice(0, 10);
+                const number = parseInt(e.target.value, 10);
+                e.target.setCustomValidity(
+                  number >= 6000000000 && number <= 9999999999
+                    ? ""
+                    : "Enter a valid phone number"
+                );
+              }}
+            />
+          )}
+
+          <input
+            type="password"
+            placeholder="Password"
+            className="px-4 py-2 border rounded-lg focus:ring-1 focus:ring-blue-800 outline-none"
+            required
+            name="password"
+            value={userData.password}
+            onChange={handleChange}
+          />
+          <div className="flex justify-center mb-4">
+            <button
+              className={`px-4 py-1 rounded-l-lg ${
+                loginWith === "email"
+                  ? "bg-orange-600 text-white"
+                  : "bg-gray-200 text-gray-700"
+              }`}
+              onClick={() => setLoginWith("email")}
+            >
+              Email
+            </button>
+            <button
+              className={`px-4 py-1 rounded-r-lg ${
+                loginWith === "number"
+                  ? "bg-orange-600 text-white"
+                  : "bg-gray-200 text-gray-700"
+              }`}
+              onClick={() => setLoginWith("number")}
+            >
+              Phone
+            </button>
+          </div>
+
+          <button
+            type="submit"
+            className="bg-gray-500 text-white py-2 rounded-lg hover:bg-gray-700 transition-all"
+          >
+            Log-In
+          </button>
+
+          <p className="text-end text-sm">
+            <Link
+              to="/signup"
+              title="Go to Sign-Up"
+              className="hover:text-blue-500"
+            >
+              Sign-Up
+            </Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default LogIn;
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+
+function SignUp() {
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    phoneNumber: "",
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submit Clicked", userData);
+    toast.success("SignUp succesfully ");
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData((pd) => ({ ...pd, [name]: value }));
+    console.log(name, value);
+  };
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-blue-200">
+      <div className="bg-white shadow-lg rounded-xl p-8 w-96">
+        <h2 className="text-2xl font-semibold text-gray-700 text-center mb-6">
+          ✍️chatify
+        </h2>
+
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <input
+            type="text"
+            placeholder="Name"
+            className="px-4 py-2 border rounded-lg focus:ring-1 focus:ring-blue-800  outline-none"
+            required
+            autoFocus
+            name="name"
+            value={userData.name}
+            onChange={handleChange}
+          />
+
+          <input
+            type="email"
+            placeholder="Email"
+            className="px-4 py-2 border rounded-lg focus:ring-1 focus:ring-blue-800 outline-none"
+            required
+            name="email"
+            value={userData.email}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Password"
+            className="px-4 py-2 border rounded-lg focus:ring-1 focus:ring-blue-800 outline-none"
+            required
+            name="password"
+            value={userData.password}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Phone Number"
+            className="px-4 py-2 border rounded-lg focus:ring-1 focus:ring-blue-800 outline-none"
+            required
+            name="phoneNumber"
+            value={userData.phoneNumber}
+            onChange={handleChange}
+            onInput={(e) => {
+              e.target.value = e.target.value.replace(/\D/g, "");
+              e.target.value = e.target.value.slice(0, 10);
+              const phoneNumber = e.target.value;
+              if (phoneNumber.length === 10) {
+                const number = parseInt(phoneNumber, 10);
+                if (number < 6000000000 || number > 9999999999) {
+                  e.target.setCustomValidity(
+                    "Phone number must be between 6000000000 and 9999999999."
+                  );
+                } else {
+                  e.target.setCustomValidity("");
+                }
+              } else {
+                e.target.setCustomValidity(
+                  "Phone number must be exactly 10 digits."
+                );
+              }
+            }}
+          />
+
+          <button
+            type="submit"
+            className="bg-gray-500 text-white py-2 rounded-lg hover:bg-gray-700 transition-all"
+          >
+            Sign Up
+          </button>
+          <p className="text-end text-sm">
+            <Link to="/login" className="hover:text-blue-300">
+              Log-In
+            </Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
